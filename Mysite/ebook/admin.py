e7244cf49b2a979c976831e4251b5269cc9875ef
aamansharma <amansh027@gmail.com>
2021-01-28 22:01:55 +0530

    Initial commit

---
+++ Mysite/ebook/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from ebook.models import Contact
+
+# Register your models here
+
+admin.site.register(Contact)
---
+++ Mysite/ebook/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from ebook.models import Contact
+
+# Create your views here.
+# admin
+#aman0542@
+
+def index(request):
+   # return HttpResponse("this is home page")
+     return render(request, 'index.html')
+
+def about(request):
+     return render(request, 'about.html')
+
+def services(request):
+     return render(request, 'services.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+
+    return render(request, 'contact.html')
+     #return render(request, 'contact.html')
